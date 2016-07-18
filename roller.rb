3ab2651c6b0e6660ca7be32bd59ec86218a26d7c
Noah Berman <noah@noahberman.org>
2016-07-17 22:14:09 -0500

    starting over

--- roller.rb
+++ roller.rb
@@ -1,19 +1,4 @@
-def roll(sides, number=1)
-  roll_array = []
-  number.times do
-    roll_value = rand(sides) + 1
-    roll_array << roll_value
-  end
-  total = 0
-  roll_array.each do |roll|
-    new_total = total + roll
-    total = new_total
-  end
-  total
+def roll
+  rand(6) + 1
 end
-
-puts "We're rolling a six sided die!"
-puts roll(6)
-
-puts "Now we're rolling two 20 sided die!"
-puts roll(20, 2)
+puts roll
